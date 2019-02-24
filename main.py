3f887732d4787b5bb639155b6470c06546a36ec1
Law Wai Chun <chunlaw@rocketmail.com>
2019-02-24 12:21:08 +0800

    Solve most of the courts

--- main.py
+++ main.py
@@ -2,24 +2,32 @@
 import sys
 import json
 import re
+import requests
 from bs4 import BeautifulSoup
 from html_table_extractor.extractor import Extractor
 
-html_doc = open( sys.argv[1], "r" )
-soup = BeautifulSoup( html_doc.read(), 'html.parser')
-tables = soup.find_all('table')
-if len(tables)<2:
-    exit
+def printJson(obj):
+    print json.dumps(obj, ensure_ascii=False, indent=4)
 
-def unique(sequence):
-    seen = set()
-    return [x for x in sequence if not (x in seen or seen.add(x))]
+class CourtParser:
+    headers = []
 
-contents = []
-headers = []
-for table in tables:
-    # parse the data after if the header is set
-    if headers:
+    def parseHeader(self, table):
+        if "案件編號" in str(table) or "案件號碼" in str(table):
+            extractor = Extractor( str(table), transformer=unicode)
+            extractor.parse()
+            headerTable = extractor.return_list()
+            for i in xrange(0,len(headerTable)):
+                if (''.join(headerTable[i])).find(u'案件號碼') != -1 or (''.join(headerTable[i])).find(u'案件編號') != -1:
+                    self.headers = headerTable[i]
+            self.headers = self.unique(self.headers)
+            for i in xrange(0, len(self.headers)):
+                self.headers[i] = re.sub('[A-Za-z]', '', self.headers[i]).strip()       
+
+    def parseTable(self, table):
+        self.parseHeader(table)
+        if self.headers == []:
+            return []
         extractor = Extractor( str(table), transformer=unicode)
         extractor.parse()
         raw_data = extractor.return_list()
@@ -28,14 +36,16 @@ for table in tables:
             for j in xrange(0, len(raw_data[i])):
                 if raw_data[i][j].strip() == '':
                     raw_data[i][j] = raw_data[i-1][j]
+        
+        raw_data = [a for a in raw_data if (''.join(a)).find(u'上午') != -1 or (''.join(a)).find(u'下午') != -1]
 
         # assigning data with header as key
         data = []
         for i in xrange(0,len(raw_data)):
             data.append({})
-            raw_data[i] = unique (raw_data[i])   # handle colspan here
+            raw_data[i] = self.unique (raw_data[i])   # handle colspan here
             for j in xrange(0, len(raw_data[i])):       
-                data[i][headers[0][j]] = raw_data[i][j]
+                data[i][self.headers[j]] = raw_data[i][j]
 
         # merging cells content
         content = []
@@ -43,24 +53,52 @@ for table in tables:
             if not ( u'案件號碼' in data[i] or u'案件編號' in data[i]):
                 continue
             if content == [] or \
-                ( u'案件號碼' in data[i-1] and data[i-1][u'案件號碼'].strip() != data[i][u'案件號碼'].strip() ) or \
-                ( u'案件編號' in data[i-1] and data[i-1][u'案件編號'].strip() != data[i][u'案件編號'].strip() ):
+                ( u'案件號碼' in data[i] and content[-1][u'案件號碼'].strip() != data[i][u'案件號碼'].strip() ) or \
+                ( u'案件編號' in data[i] and content[-1][u'案件編號'].strip() != data[i][u'案件編號'].strip() ):
                 content.append(data[i])
             else:
-                #print data[i-1][u'案件編號'].strip() != data[i][u'案件編號'].strip()
+                #print json.dumps(content[-1], ensure_ascii=False, indent=4)
+                #print json.dumps(data[i], ensure_ascii=False, indent=4)
                 for k, v in data[i].iteritems():
-                    if data[i-1][k] != data[i][k]:
+                    if content[-1][k] != data[i][k]:
                         content[-1][k] += data[i][k]
-        contents.append(content)
-        headers = []
-    # parse the header
-    if "案件編號" in str(table) or "案件號碼" in str(table):
-        extractor = Extractor( str(table), transformer=unicode)
-        extractor.parse()
-        headers = extractor.return_list()
-        headers[0] = unique(headers[0])
-        for i in xrange(0, len(headers[0])):
-            headers[0][i] = re.sub('[A-Za-z]', '', headers[0][i]).strip()
+        return content
 
+    def parseSite(self,htmlText):
+        soup = BeautifulSoup( htmlText, 'html.parser')
+        tables = soup.find_all('table')
+        if len(tables)<2:
+            exit
 
-print json.dumps(contents, ensure_ascii=False)
+        contents = []
+        for table in tables:
+            # parse the data after if the header is set
+            content = self.parseTable(table)
+            if content != []:
+                contents.append(content)
+        return contents
+
+    def unique(self, sequence):
+        seen = set()
+        return [x for x in sequence if not (x in seen or seen.add(x))]
+
+codes = ["cfa","cacfi","hcmc","mcl","bp","clpi","clcmc","crhpi","cwup","mia","otd","o14","ct","lands","dc","dcmc","etnmag","kcmag","ktmag","wkmag","stmag","flmag","tmmag","allmag","crc","lt","smt","oat"]
+special_codes = ['fmc'] # TODO handle this
+url_base = "https://e-services.judiciary.hk/dcl/view.jsp"
+url_header = {'User-Agent': 'Mozilla/5.0'}
+
+text = ''
+if len(sys.argv) == 2:
+    f = open(sys.argv[1],'r')
+    text = f.read()
+    cp = CourtParser()
+    print json.dumps(cp.parseSite(text), ensure_ascii=False, indent=4)
+else:
+    cases = []
+    for code in codes:
+        code = code.upper()
+        r = requests.get(url_base, headers = url_header, params = {"lang": "en", 'date': '25022019', 'court': code})
+        text = r.text
+        cp = CourtParser()
+        cases.append([code, cp.parseSite(text)])
+    print json.dumps(cases, ensure_ascii=False, indent=4)
